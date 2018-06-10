01d58d0c1536e16b974b9d4304c4a902bf2b3870
Kyle Lin <kylejlin.us@gmail.com>
2018-06-10 15:27:06 -0700

    Updates

--- scripts/build.sh
+++
@@ -1,4 +0,0 @@
-cargo +nightly build --release --target wasm32-unknown-unknown \
-&& wasm-bindgen target/wasm32-unknown-unknown/release/nzsc_web_cli.wasm --out-dir ./web-src/wasm/ \
-&& npx webpack \
-&& cp -a ./public/. ./build/
--- scripts/dev.sh
+++
@@ -1,4 +0,0 @@
-cargo +nightly build --release --target wasm32-unknown-unknown \
-&& wasm-bindgen target/wasm32-unknown-unknown/release/nzsc_web_cli.wasm --out-dir ./web-src/wasm/ \
-&& cp -a ./public/. ./build/ \
-&& npx webpack --config ./webpack.dev.config.js & serve build -l 3000
