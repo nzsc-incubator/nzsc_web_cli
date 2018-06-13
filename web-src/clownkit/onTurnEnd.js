10602c34364324de5226b8ac3342a16b8b72f4bf
Kyle Lin <kylejlin.us@gmail.com>
2018-06-13 16:06:53 -0700

    Fix bug in onTurnEnd by only emitting if state is an appropriate state

--- web-src/clownkit/onTurnEnd.js
+++ web-src/clownkit/onTurnEnd.js
@@ -1,3 +1,5 @@
+import { NONE_VIEWED, A_VIEWED, B_VIEWED } from './helpers/consts';
+
 const onTurnEnd = (firebase, roomName, callback) => {
   const db = firebase.firestore();
   const guardianRef = db.collection('guardians').doc(roomName);
@@ -13,7 +15,10 @@ const onTurnEnd = (firebase, roomName, callback) => {
 
     async (guardianDoc) => {
       const { state } = guardianDoc.data();
-      if (guardianDoc.hasPendingWrites ||  state === cachedState) {
+      if (guardianDoc.hasPendingWrites
+        || state === cachedState
+        || ![NONE_VIEWED, A_VIEWED, B_VIEWED].includes(state)
+      ) {
         return;
       }
       const [aDoc, bDoc] = await Promise.all([avRef.get(), bvRef.get()]);
